ca11180fd2b84c4918d3b1220f3c344ba8efb927
Brian Glusman <brian.glusman@pepsico.com>
2023-06-16 10:43:11 -0400

    v1 attempt to add Falcon as direct copy from Llama config

---
+++ test/bumblebee/text/falcon_test.exs
@@ -0,0 +1,88 @@
+defmodule Bumblebee.Text.FalconTest do
+  use ExUnit.Case, async: false
+
+  import Bumblebee.TestHelpers
+
+  @moduletag model_test_tags()
+
+  describe "integration" do
+    test "base model" do
+      assert {:ok, %{model: model, params: params, spec: spec}} =
+               # TODO - fix model test location/create
+               Bumblebee.load_model({:hf, "seanmor5/tiny-falcon-test"}, architecture: :base)
+
+      assert %Bumblebee.Text.falcon{architecture: :base} = spec
+
+      input_ids = Nx.tensor([[1, 15043, 3186, 825, 29915, 29879, 701]])
+
+      inputs = %{
+        "input_ids" => input_ids
+      }
+
+      outputs = Axon.predict(model, params, inputs)
+
+      assert Nx.shape(outputs.hidden_state) == {1, 7, 32}
+
+      assert_all_close(
+        outputs.hidden_state[[.., 1..3, 1..3]],
+        Nx.tensor([
+          [[-0.4411, -1.9037, 0.9454], [0.8148, -1.4606, 0.0076], [0.9480, 0.6038, 0.1649]]
+        ]),
+        atol: 1.0e-2
+      )
+    end
+
+    test "sequence classification model" do
+      assert {:ok, %{model: model, params: params, spec: spec}} =
+               Bumblebee.load_model(
+                # TODO - fix model test location/create
+                 {:hf, "HuggingFaceH4/tiny-random-falconForSequenceClassification"}
+               )
+
+      assert %Bumblebee.Text.falcon{architecture: :for_sequence_classification} = spec
+      input_ids = Nx.tensor([[1, 15043, 3186, 825, 29915, 29879, 701]])
+
+      inputs = %{
+        "input_ids" => input_ids
+      }
+
+      outputs = Axon.predict(model, params, inputs)
+
+      assert Nx.shape(outputs.logits) == {1, 1}
+
+      assert_all_close(
+        outputs.logits,
+        Nx.tensor([[-0.0977]]),
+        atol: 1.0e-4
+      )
+    end
+
+    test "causal language model" do
+      assert {:ok, %{model: model, params: params, spec: spec}} =
+               # TODO - fix model test location/create
+               Bumblebee.load_model({:hf, "seanmor5/tiny-falcon-test"},
+                 architecture: :for_causal_language_modeling
+               )
+
+      assert %Bumblebee.Text.falcon{architecture: :for_causal_language_modeling} = spec
+
+      input_ids = Nx.tensor([[1, 15043, 3186, 825, 29915, 29879, 701]])
+
+      inputs = %{
+        "input_ids" => input_ids
+      }
+
+      outputs = Axon.predict(model, params, inputs)
+
+      assert Nx.shape(outputs.logits) == {1, 7, 32000}
+
+      assert_all_close(
+        outputs.logits[[.., 1..3, 1..3]],
+        Nx.tensor([
+          [[0.0592, 0.1188, -0.1214], [-0.0331, 0.0335, -0.1808], [-0.1825, -0.0711, 0.0497]]
+        ]),
+        atol: 1.0e-2
+      )
+    end
+  end
+end
